a0092b294509adac6f72fa5b3ad6ab789a8ee7f6
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-07-13 02:09:43 +0530

    change the order status is  done. ✔

--- controllers/orderCntroller.js
+++ controllers/orderCntroller.js
@@ -1,5 +1,6 @@
 import Order from "../models/order.js"
 import Product from "../models/products.js"
+import { isAdmin } from "./userController.js"
 
 export async function crateOrder(req,res){
     //get user information
@@ -123,4 +124,38 @@ export async function getOrders(req,res){
             error: err
         })
     }
+}
+
+
+export async function updateOrderStatus(req,res) {
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message: "You are not authorized to update order status",
+        })
+        return
+    }
+    try{
+        const orderId = req.params.orderId
+        const status = req.params.status
+
+        await Order.updateOne(
+            {
+                orderId : orderId
+            },
+            {
+                status : status 
+            }
+        )
+        res.json({
+            message: "Order status updated successfully",
+        })
+
+    }catch(e){
+        res.status(500).json({
+            message: "Failed to update order status",
+            error: e,
+        })
+        return
+    }
+        
 }
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { crateOrder, getOrders } from '../controllers/orderCntroller.js';
+import { crateOrder, getOrders, updateOrderStatus } from '../controllers/orderCntroller.js';
 
 const orderRouter  = express.Router();
 
 orderRouter.post("/",crateOrder)
 orderRouter.get("/",getOrders)
-
+orderRouter.put("/:orderId/:status",updateOrderStatus)
 
 export default orderRouter;
