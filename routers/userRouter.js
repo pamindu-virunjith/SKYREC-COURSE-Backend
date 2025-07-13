3522100471513c43f929dd743f605532f7354683
Pamindu Virunjith <paminduvirunjith2002@gmail.com>
2025-07-14 00:36:26 +0530

    reset password task is done. ✔

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, loginUser, loginWithGoole, resetPassword, sendOTP } from "../controllers/userController.js";
 
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login",loginUser)
+userRouter.post("/login/google", loginWithGoole)
+userRouter.post("/send-otp", sendOTP)
+userRouter.post("/reset-password", resetPassword)
 
 export default userRouter;
